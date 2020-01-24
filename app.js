80b866fec31cbbfba8d51df0233fe00d33064c51
ilayre <44551308+ilayre@users.noreply.github.com>
2020-01-24 11:53:36 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require('express-handlebars');
+const nodemailer = require('nodemailer');
+const path = require('path');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+	res.render("index");
+});
+
+app.get("/gallery", function(req, res){
+	res.render("gallery");
+});
+
+app.get("/product", function(req, res){
+	res.render("product");
+});
+
+app.get("/contact_send", function(req, res){
+	res.render("contact_send");
+});
+
+app.get("/contact_error", function(req, res){
+	res.render("contact_error");
+});
+
+app.post("/contact_send", function(req, res){
+	const output = `
+		<P>new contact request</p>
+		<h3>details</h3>
+		<ul>
+			<li>Email: ${req.body.email}</li>
+			<li>Name: ${req.body.name}</li>
+			<li>Phone: ${req.body.phone}</li>
+		</ul>
+	`;
+	// async function main() {
+	// 	// Generate test SMTP service account from ethereal.email
+	// 	// Only needed if you don't have a real mail account for testing
+	// 	let testAccount = await nodemailer.createTestAccount();
+
+	let transporter = nodemailer.createTransport({
+		service: 'gmail',
+		// port: 587,
+   		// secure: false,
+		auth: {
+		  user: 'barber.dog.contact@gmail.com',
+		  pass: 'qFgSbKwCfh5urhD' 
+		}
+	});
+	
+	  // send mail with defined transport object
+	let mailOptions = {
+		from: 'barber.dog.contact@gmail.com', // sender address
+		to: 'orbarberdog@gmail.com', // list of receivers oruzal85@gmail.com orbarberdog@gmail.com
+		subject: "you have a new contact", // Subject line
+		text: "Hello", // plain text body
+		html: output // html body
+	};
+	
+	transporter.sendMail(mailOptions, function(err, data) {
+		if(err){
+			console.log(err);
+			return res.render("contact_error");
+		} else{
+			console.log('email sent!');
+			return res.render("contact_send");
+		}
+	});
+
+// }
+});
+
+	
+app.listen(3000, function(){
+	console.log("server listen on port 3000");
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+	console.log("Server has started");
+});
